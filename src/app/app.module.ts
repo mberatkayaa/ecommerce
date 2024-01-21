2edf66599598e70903579061fdb9f0548485c4ba
Takezou0 <bkaya1228@gmail.com>
2024-01-22 01:11:30 +0300

    Update app component and modules to try new features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { DropdownTogglerDirective } from "./shared/directives/dropdown-toggler.directive";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { IconsService } from "./shared/services/icons.service";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HeaderComponent, HomePageComponent, DropdownTogglerDirective, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  providers: [IconsService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
