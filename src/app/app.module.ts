bbcf96022121cda4e384916aff5f9a27a4a78502
Takezou0 <bkaya1228@gmail.com>
2024-01-22 19:51:22 +0300

    Add OwlCarousel and a demo carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -9,10 +10,11 @@ import { DropdownTogglerDirective } from "./shared/directives/dropdown-toggler.d
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { IconsService } from "./shared/services/icons.service";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { CarouselModule } from "ngx-owl-carousel-o";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, HomePageComponent, DropdownTogglerDirective, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, BrowserAnimationsModule, CarouselModule],
   providers: [IconsService],
   bootstrap: [AppComponent],
 })
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener, QueryList, ViewChild, ViewChildren } from "@angular/core";
+import { CarouselComponent, OwlOptions } from "ngx-owl-carousel-o";
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  selector: "app-home-page",
+  templateUrl: "./home-page.component.html",
+  styleUrl: "./home-page.component.css",
 })
 export class HomePageComponent {
+  @ViewChild("carousel") carousel: CarouselComponent;
+  @HostListener("window:resize", ["$event"])
+  resizeHandler() {
+    this.customOptions = { ...this.customOptions };
+    this.carousel.ngOnInit();
+  }
 
+  customOptions: OwlOptions = {
+    loop: true,
+    center: true,
+    items: 1,
+    margin: 0,
+    autoplay: true,
+    dots: true,
+    autoplayTimeout: 2500,
+    smartSpeed: 450,
+    nav: false,
+    autoWidth: true,
+    responsive: {},
+  };
 }
